d69094106a40104757922afebc5284a2e9cb36a2
Miguel Trejo <miguel.trejo@upeu.edu.pe>
2024-04-23 23:19:29 -0500

    nav

--- src/pages/public/home/Home.jsx
+++ src/pages/public/home/Home.jsx
@@ -2,9 +2,17 @@ import Navbar from "../../../shared/navbar/public/Navbar";
 
 export default function Home() {
   return (
-    <div>
+    <div className="container-fluid ">
         <Navbar />
-        <h1>Estas en home</h1>
+        <div className="flex-grow-1">
+            <div className="container">
+                <div className="row justify-content-center align-items-center">
+                    <div className="col container-fluid">
+                        <h1 className="text-center">Estás en Home</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
